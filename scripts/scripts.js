b292001c0dec6c1e6d27f6013ccaa44e7b78d58c
Thomas Walker <u0717987@ad.utah.edu>
2017-01-19 12:34:37 -0700

    pushing because i'm leaving

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,29 +2,30 @@ var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
 var x = 0, y = 0;
 var img = new ImageData(1, 1);
+var colorValue = 255;
 
 $(document).ready(function() {
     $('#canvas').prop("width", $('.name-type').width());
     console.log("ready");
-    // setInterval(function() {
-    //     updateCanvas();
-    //     renderCanvas();
-    // }, 100);
+//     setInterval(function() {
+//         updateCanvas();
+//         renderCanvas();
+//     }, 100);
     updateCanvas();
     renderCanvas();
 });
 
 function updateCanvas() {
 
-    for(var i = 0; i < 250; i++) {
-        img.data[0] = 255;
-        img.data[1] = 255;
-        img.data[2] = 255;
-        img.data[3] = 255;
-    }
+    img.data[0] = colorValue;
+    img.data[1] = colorValue;
+    img.data[2] = colorValue;
+    img.data[3] = 255;
+    
 }
 
 function renderCanvas() {
+    //c.clearRect(0,0, canvas.width, canvas.height);
     var nameTypeW = $('.name-type').width();
     while(x < nameTypeW/2) {
         c.putImageData(img, x+=2, y+=1);
