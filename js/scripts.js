471fb41d5225f2b74079e4a37a8f371740bee780
Thomas <thomasedwalker@gmail.com>
2019-06-10 22:31:50 -0600

    new ideas

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,14 @@
-$(function () {
-    setTimeout(function() {
-        tada();
-    }, 900);
-    // setTimeout(function () { typeThis("Thomas Walker", $("#header-name"))}, 900);
-    //setTimeout(function() {showContentContainer()}, 2100);
+var p1 = 30;
+
+document.addEventListener('DOMContentLoaded', function() {
+	console.log("ready");
+	document.getElementById('part-one').style.paddingTop = p1 + "vh";
 });
 
-function tada() {
-    typeThis("Thomas Walker", $("#header-name"));
-    setTimeout(function() {
-        showContentContainer();
-    },2100);
-}
-
-function typeThis(text, element) {
-    var index = 0;
-
-    var typer = setInterval(function () {
-        if (index === text.length) {
-            clearInterval(typer);
-            //console.log("done");
-        } else {
-            element.append(text.charAt(index));
-            index++;
-        }
-    }, 50);
-}
-
-function showContentContainer() {
-    var height = $("#content-container").height();
-    $("#content-container").css({"height" : "0px", "visibility": "visible"});
-    $("#content-container").animate({"height" : height + "px"}, 300, "linear");
-}
+// TODO mobile...
+window.addEventListener('wheel', function(e) {
+	var mod = e.deltaY > 0 ? 2 : -2;
+	var partOne = document.getElementById("part-one");
+	p1 = p1 + mod > 0 && p1 + mod <= 30 ? p1 + mod : p1;
+	partOne.style.paddingTop = p1 + "vh";
+});
