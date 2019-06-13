8417e4575a839eee6cc4b3d3ce82aeee503bd665
Thomas <thomasedwalker@gmail.com>
2019-06-12 19:17:50 -0600

    Sinelines improvements

--- p5/scripts/sinelines.js
+++ p5/scripts/sinelines.js
@@ -4,8 +4,10 @@ var nextX, nextY;
 var angle;
 var angleMod;
 var r, g, b;
-var rMod = 0.1, gMod = 0.3, bMod = 0.2;
+var rMod = 0.05, gMod = 0.1, bMod = 0.09;
 var weight = 30;
+var weightMod = 0.01;
+var gap = 30;
 
 function setup() {
 	canvasWidth = 800;
@@ -26,45 +28,33 @@ function setup() {
 function draw() {
 	clear();
 	background(55);
-	for (currentY = 10; currentY < canvasWidth + weight; currentY += (weight * 2)) {
-		currentX = 0;
-		angle = 0 + angleMod;
-		while (currentX < canvasWidth) {
-			// mess around with sine/angle modifiers for interesting things
-			nextY = currentY + Math.sin(8 * angle) * 2;
-			nextX = currentX + 4;
-			line(currentX, currentY, nextX, nextY);
-			angle += (Math.PI / 64);
-			currentX = nextX;
-			currentY = nextY;
+
+	currentX = 0;
+	currentY = 10;
+	angle = 0 + angleMod;
+	while (currentX < canvasWidth) {
+		nextY = currentY + Math.sin(8 * angle) * 8;
+		nextX = currentX + 10;
+		for (var count = 0; count * gap < canvasWidth; count += 2) {
+			line(currentX, currentY + (gap * count), nextX, nextY + (gap * count));
 		}
+
+		angle += (Math.PI / 64);
+		currentX = nextX;
+		currentY = nextY;
 
 		r += rMod;
 		g += gMod;
 		b += bMod;
-
 		rMod = r >= 255 || r <= 0 ? -rMod : rMod;
 		gMod = g >= 255 || g <= 0 ? -gMod : gMod;
 		bMod = b >= 255 || b <= 0 ? -bMod : bMod;
+
+		weight += weightMod;
+		weightMod = weight > 35 || weight < 15 ? -weightMod : weightMod;
+		strokeWeight(weight);
 		stroke(r, g, b);
 	}
-
-//	currentX = 0;
-//	currentY = 50;
-//	angle = 0 + angleMod;
-//	while (currentX < canvasWidth) {
-//		//nextY = currentY + Math.sin(4 * angle) / 8;
-//		nextY = currentY + Math.sin(4 * angle) / 8;
-//		nextX = currentX + 4;
-//		//for (var y = currentY; y < canvasWidth + weight; y += (weight * 4)) {
-//		for (var count = 0; count * width < canvasWidth; count++) {
-//			line(currentX, currentY * count, nextX, 50 * count);
-//			console.log(count);
-//		}
-//		angle += (Math.PI / 64);
-//		currentX = nextX;
-//		currentY = nextY;
-//	}
 
 	angleMod -= 0.01;
 }
