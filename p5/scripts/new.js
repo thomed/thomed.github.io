7d6a7ebb6905df90ab5eaf8b765a025dc18c1e37
thomas <thomasedwalker@gmail.com>
2020-02-20 15:11:46 -0700

    Them mountains.

--- p5/scripts/new.js
+++ p5/scripts/new.js
@@ -1,53 +1,107 @@
-var width, height;
-var x, y, startX, startY, nextX, nextY;
-var scaleX = 0.5;
+/**
+ * promising directions for function for northern side of canyon:
+ * y = sqrt(x) * xrt(x)
+ * y = xrt(logx)
+ */
 
-var baseWidth, baseHeight;
-var limitY;
+// canvas related
+var width, height;
+
+// drawing
+var skyColor;
+var mtnColor;
+var peakColor;
+var mtn;
+var startX, startY;
+
+class Mountain {
+	recWidth = 4; 
+	recHeight = 50;
+	heightMod = 50;
+	capStart = 34;
+	currentStep = 0;
+	maxStep = 100;
+
+	constructor(baseX, baseY) {
+		this.x = baseX;
+		this.y = baseY;
+	}
+	
+	step() {
+		if (this.currentStep > this.maxStep) {
+
+			this.currentStep = 0;
+		}
+
+		this.x += this.recWidth + 1;
+
+		this.recHeight = -Math.abs((this.currentStep - (this.maxStep / 2))) + this.heightMod;
+		this.recHeight += randomDifference(3);
+
+		this.currentStep++;
+	}
+
+	draw() {
+		rect(this.x, this.y - this.recHeight, this.recWidth, this.recHeight);
+
+		//if (this.currentStep >= this.maxStep * 0.33 && this.currentStep <= this.maxStep * 0.66) {
+		if (this.recHeight >= this.capStart) {
+			fill(peakColor);
+			//rect(this.x, this.y - this.recHeight, this.recWidth, this.recHeight * 0.2);
+			var capHeight = this.recHeight - this.capStart + randomDifference(3);
+			rect(this.x, this.y - this.recHeight, this.recWidth, capHeight);
+			fill(mtnColor);
+		}
+	}
+
+};
 
 function setup() {
 	width = window.innerWidth;
 	height = 480;
-	//startX = width / 2;
 	startX = 0;
 	startY = 0;
-	baseWidth = 50;
-	baseHeight = 250;
-	limitY = height + baseHeight;
 	x = 0;
-	y = 0;
-	nextX = x;
+	y = height / 2;
+
+	skyColor = color('#60baff');
+	mtnColor = color('#998877');
+	peakColor = color('#ddeafa');
+	mtn = new Mountain(startX, y + 50);
+	console.log(mtn);
+	
 	createCanvas(width, height);
 	frameRate(60);
+	noStroke();
+	background(skyColor);
 }
 
 function draw() {
-	// nextX += 1;
-	// nextY = (Math.pow(nextX * scaleX, 2));
-	// line(startX + x, startY + y, startX + nextX, startY + nextY);
-	// line(startX - x, startY + y, startX - nextX, startY + nextY);
-	// x = nextX;
-	// y = nextY;
-	makeShape(startX, startY, baseWidth, baseHeight);
-	startY += 30;
-
-	if (startY > limitY) {
-		startY = 0;
-		startX += 20;
+	// stop running after passing the edge of canvas
+	if (x > width) {
+		frameRate(0);
+		console.log("done");
 	}
 
-	if (startX > width) {
-		noLoop();
-	}
+//	fill(150, 130, 120);
+	fill(mtnColor);
+
+	// y = 40 * log_10(x + 1)^(log_100(x+1)
+//	recHeight = 40 * Math.pow(Math.log10(x + 1), 1 / (Math.log(x + 1) / Math.log(100)));
+
+	mtn.draw();
+	mtn.step();
 }
 
-function makeShape(startX, startY, shapeWidth, shapeHeight) {
-	beginShape();
-	vertex(startX, startY);
-	quadraticVertex(startX + shapeWidth / 2, startY - shapeHeight, startX + shapeWidth, startY);
-	endShape(CLOSE);
-}
-
+// random [0, 255]
 function randomSingle() {
 	return Math.floor(Math.random() * 255);
 }
+
+// return a random modifier between +- max to add roughness
+function randomDifference(max) {
+	var diff = Math.floor(Math.random() * max);
+	diff *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+	return diff;
+}
+
