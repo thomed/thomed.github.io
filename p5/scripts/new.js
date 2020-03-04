e2ea3a6345e95f8afb6133681caa76d551311ff1
thomas <thomasedwalker@gmail.com>
2020-03-04 15:57:17 -0700

    Tweaks to mountain generation. Draw from bottom of canvas.

--- p5/scripts/new.js
+++ p5/scripts/new.js
@@ -13,36 +13,44 @@ class Mountain {
 	recHeight = 5;		// height of initial rectangle
 	heightMod = 50;		// add to rectangle height
 	baseHeightMod = 50;	// starting heightmod
-	capStart = 34;		// rectangles taller than this will have snow
+	capStart = 40;		// rectangles taller than this will have snow
 	currentStep = 0;	// count for math fn (basically isolated x)
 	maxStep = 85;		// restart mnt after this many steps
+	absMult = 1;
 
 	constructor(baseX, baseY) {
 		this.x = baseX;
 		this.y = baseY;
+		this.currentStep = randomSingle(this.maxStep / 3);
+		this.recHeight = -Math.abs((this.absMult * this.currentStep - (this.maxStep / 2))) + this.heightMod;
 	}
 	
 	// calculate the next step of the mountain
 	step() {
-		this.x += this.recWidth + 1;
-		this.recHeight = -Math.abs((this.currentStep - (this.maxStep / 2))) + this.heightMod;
+		this.x += this.recWidth;
+		this.recHeight = -Math.abs((this.absMult * this.currentStep - (this.maxStep / 2))) + this.heightMod;
 		this.recHeight += randomDifference(3);
 		this.currentStep++;
 
-		if (this.currentStep > this.maxStep * 0.7 && randomSingle(10) > 4) {
+		if (this.currentStep > this.maxStep * 0.6 && randomSingle(10) > 4) {
 			console.log("HeightMod = " + this.heightMod + ". RecHeight = " + this.recHeight);
 			this.heightMod = this.recHeight + this.baseHeightMod - 5;
 			this.currentStep = 0;
+			this.absMult = randomSingle(2) + 1;
 		}
 	}
 
 	// draw rectangles for this step
 	draw() {
+		if (this.recHeight <= 0) {
+			return;
+		}
+
 		rect(this.x, this.y - this.recHeight, this.recWidth, this.recHeight);
 
 		// snow caps
 		if (this.recHeight >= this.capStart) {
-			var capHeight = this.recHeight - this.capStart + randomDifference(3);
+			var capHeight = this.recHeight - this.capStart + randomDifference(4);
 
 			fill(peakColor);
 			rect(this.x, this.y - this.recHeight, this.recWidth, capHeight);
@@ -63,7 +71,7 @@ function setup() {
 	skyColor = color('#60baff');
 	mtnColor = color('#998877');
 	peakColor = color('#ddeafa');
-	mtn = new Mountain(startX, y + 50);
+	mtn = new Mountain(startX, height);
 	console.log(mtn);
 	
 	createCanvas(width, height);
@@ -74,15 +82,17 @@ function setup() {
 
 function draw() {
 	// stop running after passing the edge of canvas
-	if (mtn.x > width) {
+	if (mtn.x < width) {
+		mtn.draw();
+		mtn.step();
+	} else {
 		frameRate(0);
 		console.log("done");
 	}
 
 	fill(mtnColor);
 
-	mtn.draw();
-	mtn.step();
+
 }
 
 // random [0, max]
