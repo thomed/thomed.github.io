9607bf8a85bc5289339878ea476c4985c2c782c7
Thomas <thomasedwalker@gmail.com>
2020-05-26 12:10:41 -0600

    Nodes are related diagonally. I think there's a lot of cool things that can be done using this as a starting point.

--- p5/scripts/nodes.js
+++ p5/scripts/nodes.js
@@ -22,6 +22,7 @@ class Node {
 
 	draw() {
 		point(this.x, this.y);
+//		stroke("#8fc0ff");
 	}
 }
 
@@ -31,13 +32,13 @@ function setup() {
 	margin = 50;
 	spacing = 25;
 
-	// Create 2d array of nodes
 	var x = margin;
 	var y = margin;
+
+	// initialize 2d array of nodes
 	for (i = 0; y < height - margin; i++) {
 		nodes.push([]);
 		for (j = 0; x < width - margin; j++) {
-//			nodes[i][j] = new Node(x, y);
 			nodes[i].push(new Node(x, y));
 			x += spacing;
 		}
@@ -46,16 +47,38 @@ function setup() {
 		y += spacing;
 	}
 	
+	// set node relations
+	for (i = 0; i < nodes.length; i++) {
+		for (j = 0; j < nodes[0].length; j++) {
+
+			// set lower right (above bottom row, left of right edge)
+			if (i != nodes.length - 1 && j != nodes[0].length - 1) {
+				nodes[i][j].lright = nodes[i + 1][j + 1];
+			}
+
+			// set lower left (above bottom row, right of left edge)
+			if (i != nodes.length - 1 && j > 0) {
+				nodes[i][j].lleft = nodes[i + 1][j - 1];
+			}
+
+			// set upper right
+			if (i > 0 && j < nodes[0].length - 1) {
+				nodes[i][j].uright = nodes[i - 1][j + 1];
+			}
+
+			// set upper left
+			if (i > 0 && j > 0) {
+				nodes[i][j].uleft = nodes[i - 1][j - 1];
+			}
+		}
+	}
+
+	// p5 config
 	canvas = createCanvas(width, height);
 	frameRate(60);
 	background("#2b2b2b");
-	stroke("#cfcfcf");
 	stroke("#8fc0ff")
 	strokeWeight(10);
-
-	console.log(nodes);
-	console.log(nodes.length);
-	console.log(nodes[0].length);
 }
 
 function draw() {
